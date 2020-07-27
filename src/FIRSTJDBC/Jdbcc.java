098667c118271445e07235a54041dfefa1385177
Hp <rishabhrai980@gmail.com>
2020-07-27 13:03:39 +0530

    create conection

--- src/FIRSTJDBC/Jdbcc.java
+++ src/FIRSTJDBC/Jdbcc.java
@@ -1,9 +1,16 @@
 package FIRSTJDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Jdbcc {
     public static void main(String arg[]){
        try {
            Class.forName("com.mysql.cj.jdbc.Driver");
+           Connection con = DriverManager.getConnection(
+                   "jdbc:mysql://localhost:3306/demostudent",
+                   "root",
+                   "");
 
 
        }catch (Exception e){
