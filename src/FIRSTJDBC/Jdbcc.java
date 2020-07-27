de1cf886568d57ebacc0b600363d7e0a97c6468d
Hp <rishabhrai980@gmail.com>
2020-07-27 13:00:32 +0530

    create try and catch blok which we control the exception

---
+++ src/FIRSTJDBC/Jdbcc.java
@@ -0,0 +1,13 @@
+package FIRSTJDBC;
+
+public class Jdbcc {
+    public static void main(String arg[]){
+       try {
+
+
+       }catch (Exception e){
+           System.out.println();
+       }
+
+    }
+}
