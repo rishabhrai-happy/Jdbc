57c9c55caebfa7b327783539fe24facdccccf065
Hp <rishabhrai980@gmail.com>
2020-07-27 13:02:35 +0530

    load the driver

--- src/FIRSTJDBC/Jdbcc.java
+++ src/FIRSTJDBC/Jdbcc.java
@@ -3,6 +3,7 @@ package FIRSTJDBC;
 public class Jdbcc {
     public static void main(String arg[]){
        try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
 
 
        }catch (Exception e){
